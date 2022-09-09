0bdd124dcbe17405e9f6a17fc0b739e696e52cfa
danielxiong <ewall1106@gmail.com>
2022-09-09 21:39:50 +0800

    feat: online request demo

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import App from './App.vue'
 import router from './router'
 import './styles/index.scss'
 
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line
-  const { mockXHR } = require('../mock')
-  mockXHR()
-}
+// if (process.env.NODE_ENV === 'development') {
+// eslint-disable-next-line
+const { mockXHR } = require('../mock')
+mockXHR()
+// }
 
 createApp(App).use(router).mount('#app')
